53f0e053e495e16beaf1bdf2744807bda394550c
Sus-Evasion <88226999+Sus-Evasion@users.noreply.github.com>
2023-10-27 23:25:34 +0800

    Fixed radio calling that results in normal radios triggering alpha warhead

--- CustomItems/Items/WarheadController.cs
+++ CustomItems/Items/WarheadController.cs
@@ -1,12 +1,14 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="Scp1499.cs" company="Joker119">
 // Copyright (c) Joker119. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
+
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using CustomItems.Events;
 using Exiled.API.Enums;
 using Exiled.API.Features;
@@ -14,6 +16,7 @@ using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
+using Mono.CompilerServices.SymbolWriter;
 using UnityEngine;
 using VoiceChat;
 
@@ -29,27 +32,47 @@ public class WarheadController : CustomItem
     public override uint Id { get; set; } = 19;
 
     /// <inheritdoc/>
-    public override string Name { get; set; } = "Warhead Controller";
+    public override string Name { get; set; } = "REDACTED";
 
     /// <inheritdoc/>
     public override string Description { get; set; } =
-        "This Radio bypasses warhead clearance and activates countdown of alpha warhead";
+        "REDACTED";
 
     /// <inheritdoc/>
     public override float Weight { get; set; } = 1.5f;
 
-    [Description("Battery for activating alpha warhead")]
-
     /// <inheritdoc/>
+    [Description("REDACTED")]
+
     public override SpawnProperties? SpawnProperties { get; set; } = new()
     {
-        Limit = 1,
+        Limit = 2,
         DynamicSpawnPoints = new List<DynamicSpawnPoint>
         {
             new()
             {
-                Chance = 100,
-                Location = SpawnLocationType.Inside079First,
+                Chance = 5,
+                Location = SpawnLocationType.Inside049Armory,
+            },
+            new()
+            {
+                Chance = 5,
+                Location = SpawnLocationType.InsideGr18,
+            },
+            new()
+            {
+                Chance = 5,
+                Location = SpawnLocationType.Inside330Chamber,
+            },
+            new()
+            {
+                Chance = 5,
+                Location = SpawnLocationType.InsideEscapeSecondary,
+            },
+            new()
+            {
+                Chance = 25,
+                Location = SpawnLocationType.InsideLocker,
             },
         },
     };
@@ -90,7 +113,7 @@ public class WarheadController : CustomItem
 
     private void OnVoiceChatting(VoiceChattingEventArgs ev)
     {
-        if (ev.VoiceMessage.Channel == VoiceChatChannel.Radio)
+        if (ev.VoiceMessage.Channel == VoiceChatChannel.Radio && (Check(ev.Player.CurrentItem) && ev.Player.CurrentItem.Base.name == "REDACTED"))
         {
             RadioWarheadManager.TriggerEvent(ev.Player, Warhead.IsInProgress, Warhead.IsDetonated);
         }
@@ -98,6 +121,9 @@ public class WarheadController : CustomItem
 
     private void OnUsingRadio(UsingRadioBatteryEventArgs ev)
     {
-        ev.Radio.BatteryLevel = 100;
+        if (Check(ev.Item) && ev.Item.Base.name == "REDACTED")
+        {
+            ev.Radio.BatteryLevel = 100;
+        }
     }
 }
